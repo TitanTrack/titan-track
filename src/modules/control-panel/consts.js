fc7b1244d2189f978ab4e7f2528e0c392dc8ae2f
Kerem Kazan <kerem.kazan@gmail.com>
2017-10-16 11:50:39 -0700

    added more pages

--- src/modules/control-panel/consts.js
+++ src/modules/control-panel/consts.js
@@ -3,6 +3,10 @@ import DashboardIcon from 'material-ui-icons/Dashboard';
 import CheckIcon from 'material-ui-icons/Check';
 import SettingsIcon from 'material-ui-icons/Settings';
 import JournalIcon from 'material-ui-icons/LibraryBooks';
+import TimeBudgetIcon from 'material-ui-icons/Schedule';
+import LearnIcon from 'material-ui-icons/School';
+import CommunityIcon from 'material-ui-icons/Group';
+import TrackersIcon from 'material-ui-icons/Explore';
 
 export const navItems = [{
   specialNavItem: 'profile',
@@ -15,15 +19,35 @@ export const navItems = [{
   path: '/',
   activeRegex: '/',
 }, {
+  label: 'Time Budget',
+  icon: <TimeBudgetIcon />,
+  path: '/time-budget',
+  activeRegex: '/time-budget',
+}, {
   label: 'Todos',
   icon: <CheckIcon />,
   path: '/todos',
   activeRegex: '/todos',
 }, {
+  label: 'Trackers',
+  icon: <TrackersIcon />,
+  path: '/trackers',
+  activeRegex: '/trackers',
+}, {
   label: 'Journals',
   icon: <JournalIcon />,
   path: '/journals',
   activeRegex: '/journals',
+}, {
+  label: 'Learn',
+  icon: <LearnIcon />,
+  path: '/learn',
+  activeRegex: '/learn',
+}, {
+  label: 'Community',
+  icon: <CommunityIcon />,
+  path: '/community',
+  activeRegex: '/community',
 }, {
   label: 'Settings',
   icon: <SettingsIcon />,
