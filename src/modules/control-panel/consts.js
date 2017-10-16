d7a18b48605ba62ba946fefb82fa0236df295855
Kerem Kazan <kerem.kazan@gmail.com>
2017-10-16 15:01:47 -0700

    added all base modules

--- src/modules/control-panel/consts.js
+++ src/modules/control-panel/consts.js
@@ -7,6 +7,10 @@ import TimeBudgetIcon from 'material-ui-icons/Schedule';
 import LearnIcon from 'material-ui-icons/School';
 import CommunityIcon from 'material-ui-icons/Group';
 import TrackersIcon from 'material-ui-icons/Explore';
+import GoalsIcon from 'material-ui-icons/FitnessCenter';
+import ScheduleIcon from 'material-ui-icons/DateRange';
+import LogsIcon from 'material-ui-icons/Note';
+import FlowchartsIcon from 'material-ui-icons/CallSplit';
 
 export const navItems = [{
   specialNavItem: 'profile',
@@ -19,10 +23,20 @@ export const navItems = [{
   path: '/',
   activeRegex: '/',
 }, {
+  label: 'Goals',
+  icon: <GoalsIcon />,
+  path: '/goals',
+  activeRegex: '/goals',
+}, {
   label: 'Time Budget',
   icon: <TimeBudgetIcon />,
   path: '/time-budget',
   activeRegex: '/time-budget',
+}, {
+  label: 'Schedule',
+  icon: <ScheduleIcon />,
+  path: '/schedule',
+  activeRegex: '/schedule',
 }, {
   label: 'Todos',
   icon: <CheckIcon />,
@@ -34,6 +48,11 @@ export const navItems = [{
   path: '/trackers',
   activeRegex: '/trackers',
 }, {
+  label: 'Logs',
+  icon: <LogsIcon />,
+  path: '/logs',
+  activeRegex: '/logs',
+}, {
   label: 'Journals',
   icon: <JournalIcon />,
   path: '/journals',
@@ -43,6 +62,11 @@ export const navItems = [{
   icon: <LearnIcon />,
   path: '/learn',
   activeRegex: '/learn',
+}, {
+  label: 'Flow Charts',
+  icon: <FlowchartsIcon />,
+  path: '/flowcharts',
+  activeRegex: '/flowcharts',
 }, {
   label: 'Community',
   icon: <CommunityIcon />,
